797ebdb34b57fe7b336febf8b218a462177b52a9
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-03-01 22:32:52 +0100

    Updated the main game loop

--- Minigin/Minigin.cpp
+++ Minigin/Minigin.cpp
@@ -5,6 +5,9 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include "Minigin.h"
+
+#include <chrono>
+
 #include "InputManager.h"
 #include "SceneManager.h"
 #include "Renderer.h"
@@ -85,8 +88,22 @@ void dae::Minigin::Run(const std::function<void()>& load)
 
 	// todo: this update loop could use some work.
 	bool doContinue = true;
+	auto lastTime = std::chrono::high_resolution_clock::now();
+	float lag = 0.0f;
 	while (doContinue)
 	{
+		constexpr float fixedTimeStep = 0.02f;
+		const auto currentTime = std::chrono::high_resolution_clock::now();
+		const float deltaTime = std::chrono::duration<float>(currentTime - lastTime).count();
+		lastTime = currentTime;
+		lag += deltaTime;
+
+		while (lag >= fixedTimeStep)
+		{
+			// Fixed Update
+			lag -= fixedTimeStep;
+		}
+
 		doContinue = input.ProcessInput();
 		sceneManager.Update();
 		renderer.Render();
