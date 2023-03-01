5c46a8b88a1d54c059c78141493bbe1b857dcbfd
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-03-01 22:32:39 +0100

    Implemented Component system
    Gameobjects make use of components instead of inheritance

--- Minigin/GameObject.cpp
+++ Minigin/GameObject.cpp
@@ -1,24 +1,27 @@
-#include <string>
 #include "GameObject.h"
 #include "ResourceManager.h"
-#include "Renderer.h"
 
 dae::GameObject::~GameObject() = default;
 
-void dae::GameObject::Update(){}
+void dae::GameObject::Update() {}
 
 void dae::GameObject::Render() const
 {
-	const auto& pos = m_transform.GetPosition();
-	Renderer::GetInstance().RenderTexture(*m_texture, pos.x, pos.y);
+	//const auto& pos = m_transform.GetPosition();
+	//Renderer::GetInstance().RenderTexture(*m_texture, pos.x, pos.y);
+	for (const auto& pComponent : m_ComponentPtrs)
+	{
+		if (pComponent->CanRender()) 
+			pComponent->Render();
+	}
 }
 
-void dae::GameObject::SetTexture(const std::string& filename)
-{
-	m_texture = ResourceManager::GetInstance().LoadTexture(filename);
-}
+//void dae::GameObject::SetTexture(const std::string& filename)
+//{
+//	m_texture = ResourceManager::GetInstance().LoadTexture(filename);
+//}
 
-void dae::GameObject::SetPosition(float x, float y)
-{
-	m_transform.SetPosition(x, y, 0.0f);
-}
+//void dae::GameObject::SetPosition(float x, float y)
+//{
+//	//m_transform.SetPosition(x, y, 0.0f);
+//}
--- Minigin/GameObject.h
+++ Minigin/GameObject.h
@@ -1,31 +1,85 @@
 #pragma once
 #include <memory>
-#include "Transform.h"
+#include <vector>
+#include <stdexcept>
+
+#include "Component.h"
 
 namespace dae
 {
 	class Texture2D;
-
+	class Component;
 	// todo: this should become final.
-	class GameObject 
+	class GameObject final
 	{
 	public:
-		virtual void Update();
-		virtual void Render() const;
-
-		void SetTexture(const std::string& filename);
-		void SetPosition(float x, float y);
-
 		GameObject() = default;
-		virtual ~GameObject();
+		~GameObject();
 		GameObject(const GameObject& other) = delete;
+		GameObject& operator=(const GameObject& rhs) = delete;
 		GameObject(GameObject&& other) = delete;
-		GameObject& operator=(const GameObject& other) = delete;
-		GameObject& operator=(GameObject&& other) = delete;
+		GameObject& operator=(GameObject&& rhs) = delete;
+
+		void Update();
+		void Render() const;
+
+		template <class T, typename Owner>
+		void AddComponent(const Owner& owner);
+		template <class T>
+		std::shared_ptr<T> GetComponent();
+		template <class T>
+		void RemoveComponent();
+		template <class T>
+		bool HasComponent() const;
 
 	private:
-		Transform m_transform{};
-		// todo: mmm, every gameobject has a texture? Is that correct?
-		std::shared_ptr<Texture2D> m_texture{};
+		std::vector<std::shared_ptr<Component>> m_ComponentPtrs;
 	};
+
+	template <class T, typename Owner>
+	void GameObject::AddComponent(const Owner& owner)
+	{
+		if (HasComponent<T>())
+			throw std::runtime_error("There is already a component of this type connected to this GameObject");
+
+		auto pComponent = std::make_shared<T>(owner);
+		m_ComponentPtrs.push_back(pComponent);
+		delete pComponent;
+		//return pComponent;
+	}
+
+	template <class T>
+	std::shared_ptr<T> GameObject::GetComponent()
+	{
+		if (!HasComponent<T>())
+			throw std::runtime_error("This GameObject doesn't have a component of this type");
+
+		for (const auto& pComponent : m_ComponentPtrs)
+		{
+			if (std::shared_ptr<T> derivedComponent = std::dynamic_pointer_cast<T>(pComponent))
+				return derivedComponent;
+		}
+		return nullptr;
+	}
+
+	template <class T>
+	void GameObject::RemoveComponent()
+	{
+		if (HasComponent<T>())
+			return;
+
+		// REMOVE COMPONENT LOGIC
+	}
+
+	template <class T>
+	bool GameObject::HasComponent() const
+	{
+		for (const auto& pComponent : m_ComponentPtrs)
+		{
+			if (std::dynamic_pointer_cast<T>(pComponent))
+				return true;
+		}
+
+		return false;
+	}
 }
