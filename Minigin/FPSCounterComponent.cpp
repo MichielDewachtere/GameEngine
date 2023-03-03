62d0b0b62314c35744038c87c16721324d812597
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-03-03 20:11:55 +0100

    Created a FPSCounterComponent

---
+++ Minigin/FPSCounterComponent.cpp
@@ -0,0 +1,24 @@
+#include "FPSCounterComponent.h"
+
+#include "TextComponent.h"
+#include "Time.h"
+
+void dae::FPSCounterComponent::Update()
+{
+	const auto pTextComponent = this->GetOwner().lock()->GetComponent<TextComponent>();
+
+	if (!pTextComponent)
+		return;
+
+	const float dt = Time::GetInstance().GetElapsed();
+	m_Time += dt;
+
+	if (m_Time >= m_UpdateInterval)
+	{
+		const int fps = int(1 / dt);
+
+		pTextComponent->SetText("FPS: " + std::to_string(fps));
+
+		m_Time = 0;
+	}
+}
---
+++ Minigin/FPSCounterComponent.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Component.h"
+
+namespace dae
+{
+	class FPSCounterComponent final : public Component
+	{
+	public:
+		explicit FPSCounterComponent(const std::weak_ptr<GameObject>& pOwner) : Component(pOwner) {}
+		virtual ~FPSCounterComponent() override = default;
+		FPSCounterComponent(const FPSCounterComponent& other) = delete;
+		FPSCounterComponent& operator=(const FPSCounterComponent& other) = delete;
+		FPSCounterComponent(FPSCounterComponent&& other) = delete;
+		FPSCounterComponent& operator=(FPSCounterComponent&& other) = delete;
+
+		void Update() override;
+
+		bool CanRender() const override { return false; }
+	private:
+		const float m_UpdateInterval{ 0.1f };
+		float m_Time{ m_UpdateInterval };
+	};
+}
