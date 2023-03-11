82385fdab24ae7086366d58003f75a3d40dd4712
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-03-11 14:43:28 +0100

    Updated Scene class
    Created a CreateGameObject function to create a new game object more easely

--- Minigin/Scene.cpp
+++ Minigin/Scene.cpp
@@ -1,5 +1,5 @@
 #include "Scene.h"
-#include "GameObject.h"
+//#include "GameObject.h"
 
 using namespace dae;
 
@@ -9,14 +9,25 @@ Scene::Scene(const std::string& name) : m_name(name) {}
 
 Scene::~Scene() = default;
 
-void Scene::Add(std::shared_ptr<GameObject> object)
+GameObject* Scene::CreateGameObject()
+{
+	auto pGameObject{ std::make_unique<GameObject>(this) };
+	pGameObject->Init();
+
+	GameObject* pGameObjectPtr{ pGameObject.get() };
+
+	m_objects.push_back(std::move(pGameObject));
+	return pGameObjectPtr;
+}
+
+void Scene::Add(std::unique_ptr<GameObject> object)
 {
 	m_objects.emplace_back(std::move(object));
 }
 
-void Scene::Remove(std::shared_ptr<GameObject> object)
+void Scene::Remove(std::unique_ptr<GameObject> object)
 {
-	m_objects.erase(std::remove(m_objects.begin(), m_objects.end(), object), m_objects.end());
+	std::erase(m_objects, object);
 }
 
 void Scene::RemoveAll()
