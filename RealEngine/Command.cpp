95a49732f07daf172ac60fc5837de8a093ac9f53
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-03-25 19:01:47 +0100

    Added CommandClass

---
+++ RealEngine/Command.cpp
@@ -0,0 +1,48 @@
+#include "Command.h"
+
+#include <iostream>
+
+#include "GameObject.h"
+#include "TransformComponent.h"
+#include <memory>
+
+#include "Input.h"
+#include "Time.h"
+
+#define MAX_THUMBSTICK_VALUE 32767
+
+real::MoveCommand::MoveCommand(GameObject* object, const float speed)
+	: Command(object)
+	, m_Speed(speed)
+{
+}
+void real::MoveCommand::Execute()
+{
+    // Get the TransformComponent of the game object associated with this MoveCommand.
+    const auto pTransform = GetObject()->GetComponent<TransformComponent>();
+
+    // Check if the TransformComponent is valid.
+    if (pTransform == nullptr)
+    {
+        throw std::runtime_error("no transform component found when MoveCommand::Execute executed");
+    }
+
+    // Get the current position of the game object.
+    const auto pos = pTransform->GetWorldPosition();
+
+    // Flip the y direction so that the positive y direction is up.
+    m_Direction.y *= -1;
+
+    // Calculate the new position of the game object based on its current position,
+    // its direction, and the elapsed time since the last frame.
+    glm::vec2 newPos;
+    newPos.x = pos.x + m_Direction.x * m_Speed * Time::GetInstance().GetElapsed();
+    newPos.y = pos.y + m_Direction.y * m_Speed * Time::GetInstance().GetElapsed();
+
+    // Set the new position of the game object.
+    pTransform->SetWorldPosition(newPos.x, newPos.y);
+}
+void real::TestCommand::Execute()
+{
+	std::cout << "test approved idk\n";
+}
---
+++ RealEngine/Command.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <glm/vec2.hpp>
+
+namespace real
+{
+	class GameObject;
+
+	class Command
+	{
+	public:
+		explicit Command(GameObject* object) { m_pGameObject = object; }
+		virtual ~Command() = default;
+		Command(const Command& other) = delete;
+		Command& operator=(const Command& rhs) = delete;
+		Command(Command&& other) = delete;
+		Command& operator=(Command&& rhs) = delete;
+
+		virtual void Execute() = 0;
+
+	protected:
+		GameObject* GetObject() const { return m_pGameObject; }
+
+	private:
+		GameObject* m_pGameObject;
+	};
+
+	class MoveCommand final : public Command
+	{
+	public:
+		explicit MoveCommand(GameObject* object, const float speed = 50);
+
+		void Execute() override;
+		void SetDirection(const glm::vec2& direction) { m_Direction = direction; }
+		void SetSpeed(const float speed) { m_Speed = speed; }
+
+	private:
+		glm::vec2 m_Direction{};
+		float m_Speed{};
+	};
+
+	class TestCommand final : public Command
+	{
+	public:
+		explicit TestCommand(GameObject* object) : Command(object) {}
+
+		void Execute() override;
+	};
+}
