51ad96ca0069ed1469685634d5f62302e40b651f
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-04-03 12:26:21 +0200

    Added SteamAchievements Class
    Forwards the awarded achievements to steam

---
+++ RealEngine/SteamAchievements.cpp
@@ -0,0 +1,110 @@
+#include "SteamAchievements.h"
+
+#include <isteamuser.h>
+#include <isteamuserstats.h>
+#include <isteamutils.h>
+#include <SDL_syswm.h>
+#include <stdio.h>
+
+real::SteamAchievements::SteamAchievements(Achievement* achievements, int numAchievements) 
+	: m_CallbackUserStatsReceived(this, &SteamAchievements::OnUserStatsReceived)
+    , m_CallbackUserStatsStored(this, &SteamAchievements::OnUserStatsStored)
+    , m_CallbackAchievementStored(this, &SteamAchievements::OnAchievementStored)
+    , m_AppId(0)
+    , m_Initialized(false)
+{
+    m_AppId = SteamUtils()->GetAppID();
+    m_pAchievements = achievements;
+    m_NumAchievements = numAchievements;
+    RequestStats();
+}
+
+real::SteamAchievements::~SteamAchievements() = default;
+
+bool real::SteamAchievements::RequestStats()
+{
+	// Is Steam loaded? If not we can't get stats.
+	if (NULL == SteamUserStats() || NULL == SteamUser())
+	{
+		return false;
+	}
+	// Is the user logged on?  If not we can't get stats.
+	if (!SteamUser()->BLoggedOn())
+	{
+		return false;
+	}
+	// Request user stats.
+	return SteamUserStats()->RequestCurrentStats();
+}
+
+bool real::SteamAchievements::SetAchievement(const char* ID)
+{
+	// Have we received a call back from Steam yet?
+	if (m_Initialized)
+	{
+		SteamUserStats()->SetAchievement(ID);
+		return SteamUserStats()->StoreStats();
+	}
+	// If not then we can't set achievements yet
+	return false;
+}
+
+void real::SteamAchievements::OnUserStatsReceived(UserStatsReceived_t* pCallback)
+{
+	// we may get callbacks for other games' stats arriving, ignore them
+	if (m_AppId == (int64)pCallback->m_nGameID)
+	{
+		if (k_EResultOK == pCallback->m_eResult)
+		{
+			OutputDebugString("Received stats and achievements from Steam\n");
+			m_Initialized = true;
+
+			// load achievements
+			for (int iAch = 0; iAch < m_NumAchievements; ++iAch)
+			{
+				Achievement& ach = m_pAchievements[iAch];
+
+				SteamUserStats()->GetAchievement(ach.pAchievementId, &ach.achieved);
+				_snprintf_s(ach.name, sizeof(ach.name), "%s",
+					SteamUserStats()->GetAchievementDisplayAttribute(ach.pAchievementId,
+						"name"));
+				_snprintf_s(ach.description, sizeof(ach.description), "%s",
+					SteamUserStats()->GetAchievementDisplayAttribute(ach.pAchievementId,
+						"desc"));
+			}
+		}
+		else
+		{
+			char buffer[128];
+			_snprintf_s(buffer, 128, "RequestStats - failed, %d\n", pCallback->m_eResult);
+			OutputDebugString(buffer);
+		}
+	}
+}
+
+void real::SteamAchievements::OnUserStatsStored(UserStatsStored_t* pCallback)
+{
+	// we may get callbacks for other games' stats arriving, ignore them
+	if (m_AppId == (int64)pCallback->m_nGameID)
+	{
+		if (k_EResultOK == pCallback->m_eResult)
+		{
+			OutputDebugString("Stored stats for Steam\n");
+		}
+		else
+		{
+			char buffer[128];
+			_snprintf_s(buffer, 128, "StatsStored - failed, %d\n", pCallback->m_eResult);
+			OutputDebugString(buffer);
+		}
+	}
+}
+
+void real::SteamAchievements::OnAchievementStored(UserAchievementStored_t* pCallback)
+{
+	// we may get callbacks for other games' stats arriving, ignore them
+	if (m_AppId == (int64)pCallback->m_nGameID)
+	{
+		OutputDebugString("Stored Achievement for Steam\n");
+	}
+}
---
+++ RealEngine/SteamAchievements.h
@@ -0,0 +1,47 @@
+#ifndef STEAMACHIEVEMENTS_H
+#define STEAMACHIEVEMENTS_H
+
+#include <steam_api.h>
+
+#define _ACH_ID( id, name ) { id, #id, name, "", 0, 0 }
+namespace real
+{
+	struct Achievement
+	{
+		int achievementId;
+		const char* pAchievementId;
+		char name[128];
+		char description[256];
+		bool achieved;
+		int iconImage;
+	};
+
+	class SteamAchievements final //TODO: make this a singleton?
+	{
+	public:
+		SteamAchievements(Achievement* achievements, int numAchievements);
+		~SteamAchievements();
+		SteamAchievements(const SteamAchievements& other) = delete;
+		SteamAchievements& operator=(const SteamAchievements& rhs) = delete;
+		SteamAchievements(SteamAchievements&& other) = delete;
+		SteamAchievements& operator=(SteamAchievements&& rhs) = delete;
+
+		bool RequestStats();
+		bool SetAchievement(const char* ID);
+
+		STEAM_CALLBACK(SteamAchievements, OnUserStatsReceived, UserStatsReceived_t,
+			m_CallbackUserStatsReceived);
+		STEAM_CALLBACK(SteamAchievements, OnUserStatsStored, UserStatsStored_t,
+			m_CallbackUserStatsStored);
+		STEAM_CALLBACK(SteamAchievements, OnAchievementStored,
+			UserAchievementStored_t, m_CallbackAchievementStored);
+
+	private:
+		int64 m_AppId; // Our current AppID
+		Achievement* m_pAchievements; // Achievements data
+		int m_NumAchievements; // The number of Achievements
+		bool m_Initialized; // Have we called Request stats and received the callback?
+	};
+}
+
+#endif // STEAMACHIEVEMENTS_H
