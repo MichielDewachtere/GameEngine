7ba5ebb3245c0982c288f5e1daebd924028d93ad
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-04-25 12:22:06 +0200

    Moved DamageCommand to BurgerTime

---
+++ BurgerTime/DamageCommand.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "DamageCommand.h"
+
+#include "HealthComponent.h"
+
+DamageCommand::DamageCommand(real::GameObject* object, const int damage)
+    : Command(object)
+    , m_Damage(damage)
+{
+}
+
+void DamageCommand::Execute()
+{
+    using namespace real;
+
+    const auto pHealthComponent = GetOwner()->GetComponent<HealthComponent>();
+
+    if (pHealthComponent == nullptr)
+    {
+        throw std::runtime_error("Error: Could not execute DamageCommand - no HealthComponent found on associated GameObject. Please ensure the GameObject has a HealthComponent attached.");
+    }
+
+    pHealthComponent->Damage();
+}
---
+++ BurgerTime/DamageCommand.h
@@ -0,0 +1,19 @@
+#ifndef DAMAGECOMMAND_H
+#define DAMAGECOMMAND_H
+
+#include "Command.h"
+
+class DamageCommand final : public real::Command
+{
+public:
+	explicit DamageCommand(real::GameObject* object, const int damage = 1);
+	virtual ~DamageCommand() override = default;
+
+	void Execute() override;
+
+	void SetDamageAmount(const int amount) { m_Damage = amount; }
+private:
+	int m_Damage;
+};
+
+#endif // DAMAGECOMMAND_H
