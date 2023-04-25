cca4e1cc163c470280964f23ba57eabe6ffa86f3
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-04-25 12:24:14 +0200

    Moved commands into seperate files
    Now the user of the engine has the chance to make their own commands more easely

--- RealEngine/Command.h
+++ RealEngine/Command.h
@@ -1,9 +1,6 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-#include <glm/vec2.hpp>
-#include <string>
-
 namespace real
 {
 	class GameObject;
@@ -21,69 +18,18 @@ namespace real
 		virtual void Execute() = 0;
 
 	protected:
-		GameObject* GetObject() const { return m_pGameObject; }
+		GameObject* GetOwner() const { return m_pGameObject; }
 
 	private:
 		GameObject* m_pGameObject;
 	};
 
-	class MoveCommand final : public Command
-	{
-	public:
-		explicit MoveCommand(GameObject* object, const float speed = 50);
-		virtual ~MoveCommand() override = default;
-
-		void Execute() override;
-		void SetDirection(const glm::vec2& direction) { m_Direction = direction; }
-		void SetSpeed(const float speed) { m_Speed = speed; }
-
-	private:
-		glm::vec2 m_Direction{};
-		float m_Speed{};
-	};
 	class TestCommand final : public Command
 	{
 	public:
 		explicit TestCommand(GameObject* object) : Command(object) {}
 
 		void Execute() override;
-	};
-	class DamageCommand final : public Command
-	{
-	public:
-		explicit DamageCommand(GameObject* object, const int damage = 1);
-		virtual ~DamageCommand() override = default;
-
-		void Execute() override;
-
-		void SetDamageAmount(const int amount) { m_Damage = amount; }
-	private:
-		int m_Damage;
-	};
-	//class AddPointsCommand final : public Command
-	//{
-	//public:
-	//
-	//	//TODO: wrong :0
-	//	explicit AddPointsCommand(GameObject* object, const int amount = 50);
-	//	virtual ~AddPointsCommand() override = default;
-	//
-	//	void Execute() override;
-	//
-	//	void SetPointsAmount(const int amount) { m_Amount = amount; }
-	//private:
-	//	int m_Amount;
-	//};
-	class LoadNextSceneCommand final : public Command
-	{
-	public:
-		LoadNextSceneCommand(GameObject* object, std::string name);
-		virtual ~LoadNextSceneCommand() override = default;
-
-		void Execute() override;
-
-	private:
-		std::string m_Name{ "empty" };
 	};
 }
 
