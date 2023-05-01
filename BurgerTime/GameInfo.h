b82b5e38306a8cf9ae0adea74eec834690d3974d
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-05-01 22:46:23 +0200

    Create GameInfo.h
    GameInfo with helpfull structs like tags and colors.

---
+++ BurgerTime/GameInfo.h
@@ -0,0 +1,27 @@
+#ifndef GAMEINFO_H
+#define GAMEINFO_H
+
+#include <string>
+
+struct Tags
+{
+	inline static const std::string player = "Player";
+	inline static const std::string stair = "Stair";
+	inline static const std::string floor = "Floor";
+	inline static const std::string boundary = "Boundary";
+};
+
+struct Colors
+{
+	inline static const glm::vec4 red = { 255,0,0,255 };
+	inline static const glm::vec4 green = { 0,255,0,255 };
+	inline static const glm::vec4 blue = { 0,0,255,255 };
+	inline static const glm::vec4 white = { 255,255,255,255 };
+	inline static const glm::vec4 black = { 0,0,0,255 };
+	inline static const glm::vec4 yellow = { 255,255,0,255 };
+	inline static const glm::vec4 cyan = { 0,255,255,255 };
+	inline static const glm::vec4 purple = { 255,0,255,255 };
+	inline static const glm::vec4 appelblauwzeegroen = { 0,237,116,255 };
+};
+
+#endif // GAMEINFO_H
