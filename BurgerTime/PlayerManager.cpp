b55eed2ad6b26ec93465eaf6003e7f4a3ca1bedd
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-06-06 22:21:23 +0200

    Added PlayarManager
    A singleton that handles the player and player transition between scenes.

---
+++ BurgerTime/PlayerManager.cpp
@@ -0,0 +1,93 @@
+﻿#include "PlayerManager.h"
+
+#include <ColliderComponent.h>
+#include <Input.h>
+#include <InputMap.h>
+#include <ResourceManager.h>
+#include <TextureComponent.h>
+#include <TransformComponent.h>
+
+#include "GameInfo.h"
+#include "HealthComponent.h"
+#include "LoadNextSceneCommand.h"
+#include "MoveCommand.h"
+#include "PlayerCharacter.h"
+#include "StunCommand.h"
+
+PlayerManager::~PlayerManager()
+{
+	real::SceneManager::GetInstance().onSceneLoaded.RemoveObserver(this);
+}
+
+void PlayerManager::Init()
+{
+	real::SceneManager::GetInstance().onSceneLoaded.AddObserver(this);
+}
+
+void PlayerManager::HandleEvent(real::Scene& scene)
+{
+	if (scene.GetName().find("Level") == std::string::npos)
+		return;
+
+	for (const auto& pPlayer : m_PlayerPtrs)
+	{
+		const auto playerPos = scene.FindObjectsWithTag(Tags::player_spawn)[0]->GetComponent<real::TransformComponent>()->GetWorldPosition();
+		pPlayer->GetComponent<real::TransformComponent>()->SetWorldPosition(playerPos);
+		scene.Add(pPlayer);
+	}
+}
+
+void PlayerManager::AddPlayer(bool useKeyboard)
+{
+	auto& input = real::Input::GetInstance();
+	const auto pInputMap = input.AddInputMap(InputMaps::gameplay);
+
+	std::shared_ptr<real::Texture2D> pCharacterTexture;
+
+	if (m_PlayerPtrs.empty())
+		pCharacterTexture = real::ResourceManager::GetInstance().LoadTexture("characters/PeterPepper.png");
+	else
+		pCharacterTexture = real::ResourceManager::GetInstance().LoadTexture("characters/SallySalt.png");
+
+	const auto pPepperTexture = real::ResourceManager::GetInstance().LoadTexture("characters/pepper.png");
+
+	const auto pCharacter = new real::GameObject();
+	pCharacter->SetTag(Tags::player);
+	pCharacter->SetCanBeDestroyed(false);
+	pCharacter->AddComponent<real::TransformComponent>()->SetLocalPosition(288, 423);
+	pCharacter->AddComponent<real::TextureComponent>()->SetTexture(pCharacterTexture);
+	pCharacter->AddComponent<HealthComponent>()->SetLives(4);
+	pCharacter->GetComponent<HealthComponent>()->SetSpawnPoint(pCharacter->GetComponent<real::TransformComponent>()->GetWorldPosition());
+	pCharacter->AddComponent<PlayerCharacter>();
+	pCharacter->AddComponent<real::ColliderComponent>(pCharacterTexture->GetSize())->EnableDebugRendering(true);
+
+	const auto pFeet = pCharacter->CreateGameObject();
+	pFeet->SetCanBeDestroyed(false);
+	//pFeet->SetTag(Tags::player);
+	pFeet->GetComponent<real::TransformComponent>()->SetLocalPosition(12, 0);
+	pFeet->AddComponent<real::ColliderComponent>(glm::vec2{ 24, 48 })->EnableDebugRendering(true, Colors::purple);
+	//pFeet->GetComponent<real::ColliderComponent>()->Translate(12, 0);
+
+	const auto pPepperArea = pCharacter->CreateGameObject();
+	pPepperArea->SetCanBeDestroyed(false);
+	pPepperArea->SetTag(Tags::pepper);
+	pPepperArea->SetIsActive(false);
+	pPepperArea->GetComponent<real::TransformComponent>()->SetIgnoreParent(true);
+	pPepperArea->AddComponent<real::TextureComponent>()->SetTexture(pPepperTexture);
+	pPepperArea->AddComponent<real::ColliderComponent>(pPepperTexture->GetSize())->EnableDebugRendering(true, Colors::red);
+
+
+	if (useKeyboard)
+	{
+		input.UseKeyboard(true);
+		pInputMap->AddKeyboardCommands<MoveCommand>(SDL_SCANCODE_LEFT, KEYPRESSED, pCharacter, glm::vec2{ -1, 0 });
+		pInputMap->AddKeyboardCommands<MoveCommand>(SDL_SCANCODE_RIGHT, KEYPRESSED, pCharacter, glm::vec2{ 1, 0 });
+		pInputMap->AddKeyboardCommands<MoveCommand>(SDL_SCANCODE_DOWN, KEYPRESSED, pCharacter, glm::vec2{ 0, -1 });
+		pInputMap->AddKeyboardCommands<MoveCommand>(SDL_SCANCODE_UP, KEYPRESSED, pCharacter, glm::vec2{ 0, 1 });
+		pInputMap->AddKeyboardCommands<StunCommand>(SDL_SCANCODE_Z, SDL_KEYUP, pCharacter);
+		pInputMap->AddKeyboardCommands<StunCommand>(SDL_SCANCODE_X, SDL_KEYUP, pCharacter);
+		pInputMap->AddKeyboardCommands<LoadNextSceneCommand>(SDL_SCANCODE_F1, SDL_KEYUP, pCharacter, Scenes::level02);
+	}
+
+	m_PlayerPtrs.push_back(std::shared_ptr<real::GameObject>(pCharacter));
+}
---
+++ BurgerTime/PlayerManager.h
@@ -0,0 +1,33 @@
+﻿#ifndef PLAYERMANAGER_H
+#define PLAYERMANAGER_H
+
+#include <Observer.h>
+#include <Singleton.h>
+#include <Scene.h>
+
+class PlayerManager final
+	: public real::Singleton<PlayerManager>
+	, public real::Observer<real::Scene&>
+{
+public:
+	~PlayerManager() override;
+	PlayerManager(const PlayerManager& other) = delete;
+	PlayerManager operator=(const PlayerManager& rhs) = delete;
+	PlayerManager(PlayerManager&& other) = delete;
+	PlayerManager operator=(PlayerManager&& rhs) = delete;
+
+	void Init();
+	
+	void HandleEvent(real::Scene&) override;
+	void OnSubjectDestroy() override {}
+
+	void AddPlayer(bool useKeyboard = true);
+
+private:
+	friend class Singleton<PlayerManager>;
+	PlayerManager() = default;
+
+	std::vector<std::shared_ptr<real::GameObject>> m_PlayerPtrs{};
+};
+
+#endif // PLAYERMANAGER_H
