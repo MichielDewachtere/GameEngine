5a0349af88470eee71269a9b3b7a9513c84a0b08
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-06-07 12:18:10 +0200

    Added level 03

---
+++ BurgerTime/Level03.cpp
@@ -0,0 +1,22 @@
+﻿#include "Level03.h"
+
+#include <TransformComponent.h>
+
+#include "LevelParser.h"
+#include "GameInfo.h"
+
+Level03::Level03()
+	: Scene(Scenes::level03, InputMaps::gameplay)
+{
+}
+
+void Level03::Load()
+{
+	this->SetDebugRendering(true);
+
+	const auto pLevel = LevelParser::ParseLevel(*this, "Level03.json");
+	pLevel->GetComponent<real::TransformComponent>()->Translate(48, 62);
+
+	m_IsLoaded = true;
+}
+
---
+++ BurgerTime/Level03.h
@@ -0,0 +1,19 @@
+﻿#ifndef LEVEL03_H
+#define LEVEL03_H
+
+#include <Scene.h>
+
+class Level03 final : public real::Scene
+{
+public:
+	explicit Level03();
+	virtual ~Level03() override = default;
+	Level03(const Level03& other) = delete;
+	Level03(Level03&& other) = delete;
+	Level03& operator=(const Level03& other) = delete;
+	Level03& operator=(Level03&& other) = delete;
+
+	void Load() override;
+};
+
+#endif // LEVEL03_H
