20b6c1c706d885018adb2625a3565450748944a3
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-06-07 16:17:19 +0200

    Added PlayerJoinCommand

---
+++ BurgerTime/PlayerJoinCommand.cpp
@@ -0,0 +1,17 @@
+﻿#include "PlayerJoinCommand.h"
+
+#include "PlayerManager.h"
+#include "XInputController.h"
+
+PlayerJoinCommand::PlayerJoinCommand(real::GameObject* object)
+	: Command(object)
+{
+}
+
+void PlayerJoinCommand::Execute()
+{
+	if (GetController() == nullptr)
+		PlayerManager::GetInstance().AddPlayer();
+	else
+		PlayerManager::GetInstance().AddPlayer(false, GetController()->GetIndex());
+}
---
+++ BurgerTime/PlayerJoinCommand.h
@@ -0,0 +1,19 @@
+﻿#ifndef PLAYERJOINCOMMAND_H
+#define PLAYERJOINCOMMAND_H
+
+#include <Command.h>
+
+class PlayerJoinCommand final : public real::Command
+{
+public:
+	explicit PlayerJoinCommand(real::GameObject* object);
+	virtual ~PlayerJoinCommand() override = default;
+	PlayerJoinCommand(const PlayerJoinCommand& other) = delete;
+	PlayerJoinCommand& operator=(const PlayerJoinCommand& other) = delete;
+	PlayerJoinCommand(PlayerJoinCommand&& other) = delete;
+	PlayerJoinCommand& operator=(PlayerJoinCommand&& other) = delete;
+
+	void Execute() override;
+};
+
+#endif // PLAYERJOINCOMMAND_H
