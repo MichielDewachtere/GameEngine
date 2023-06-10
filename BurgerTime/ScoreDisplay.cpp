70c3d185c4f1781397182fc40d7221ef5b7e6704
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-06-10 15:28:07 +0200

    Updated ScoreDisplay
    This is now linked with the HUD

--- BurgerTime/ScoreDisplay.cpp
+++ BurgerTime/ScoreDisplay.cpp
@@ -1,2 +1,89 @@
 //#include "stdafx.h"
 #include "ScoreDisplay.h"
+
+//#include <Scene.h>
+#include <SceneManager.h>
+#include <TextComponent.h>
+
+#include "GameInfo.h"
+#include "Ingredient.h"
+#include "BaseEnemy.h"
+
+ScoreDisplay::ScoreDisplay(real::GameObject* pOwner)
+	: Component(pOwner)
+{
+	//real::SceneManager::GetInstance().onSceneLoaded.AddObserver(this);
+	real::SceneManager::GetInstance().onSceneExit.AddObserver(this);
+}
+
+ScoreDisplay::~ScoreDisplay()
+{
+	if (real::SceneManager::GetInstance().onSceneExit.HasObserver(this))
+		real::SceneManager::GetInstance().onSceneExit.RemoveObserver(this);
+	else
+		real::SceneManager::GetInstance().onSceneLoaded.RemoveObserver(this);
+}
+
+void ScoreDisplay::Start()
+{
+}
+
+void ScoreDisplay::HandleEvent(int score)
+{
+	UpdateScoreText(score);
+}
+
+void ScoreDisplay::HandleEvent(real::Scene& scene)
+{
+	for (const auto& pIngredient	 : scene.FindObjectsWithTag(Tags::ingredient))
+	{
+		if (pIngredient->GetComponent<Ingredient>()->burgerDropped.HasObserver(this) == false)
+		{
+			pIngredient->GetComponent<Ingredient>()->burgerDropped.AddObserver(this);
+		}
+		else
+		{
+			pIngredient->GetComponent<Ingredient>()->burgerDropped.RemoveObserver(this);
+		}
+	}
+
+	std::vector<real::GameObject*> pEnemies;
+
+	for (const auto& pEnemy : scene.FindObjectsWithTag(Tags::hot_dog))
+		pEnemies.push_back(pEnemy);
+	for (const auto& pEnemy : scene.FindObjectsWithTag(Tags::pickle))
+		pEnemies.push_back(pEnemy);
+	for (const auto& pEnemy : scene.FindObjectsWithTag(Tags::egg))
+		pEnemies.push_back(pEnemy);
+
+
+	for (const auto& pEnemy : pEnemies)
+	{
+		if (pEnemy->GetComponent<BaseEnemy>()->addScore.HasObserver(this) == false)
+		{
+			pEnemy->GetComponent<BaseEnemy>()->addScore.AddObserver(this);
+		}
+		else
+		{
+			pEnemy->GetComponent<BaseEnemy>()->addScore.RemoveObserver(this);
+		}
+	}
+
+	if (real::SceneManager::GetInstance().onSceneExit.HasObserver(this))
+	{
+		real::SceneManager::GetInstance().onSceneExit.RemoveObserver(this);
+		real::SceneManager::GetInstance().onSceneLoaded.AddObserver(this);
+	}
+	else if (real::SceneManager::GetInstance().onSceneLoaded.HasObserver(this))
+	{
+		real::SceneManager::GetInstance().onSceneLoaded.RemoveObserver(this);
+		real::SceneManager::GetInstance().onSceneExit.AddObserver(this);
+	}
+}
+
+void ScoreDisplay::UpdateScoreText(int scoreToAdd)
+{
+	m_Score += scoreToAdd;
+
+	GetOwner()->GetComponent<real::TextComponent>()->SetText(std::to_string(m_Score));
+}
--- BurgerTime/ScoreDisplay.h
+++ BurgerTime/ScoreDisplay.h
@@ -4,10 +4,25 @@
 #include <Component.h>
 #include <Observer.h>
 
-class ScoreDisplay final : public real::Component, public real::Observer<>
+class ScoreDisplay final : public real::Component,
+	public real::Observer<int>,
+	public real::Observer<real::Scene&>
 {
 public:
-	//ScoreDisplay(GameObject* pOwner)
+	explicit ScoreDisplay(real::GameObject* pOwner);
+	virtual ~ScoreDisplay() override;
+
+	void Start() override;
+	void Update() override {};
+	bool CanRender() const override { return false; }
+
+	virtual void HandleEvent(int) override;
+	void HandleEvent(real::Scene&) override;
+	virtual void OnSubjectDestroy() override {}
+private:
+	int m_Score{ 0 };
+
+	void UpdateScoreText(int);
 };
 
 #endif // SCOREDISPLAY_H
