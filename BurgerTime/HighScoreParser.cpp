ee04ebfb2e88018758a54c6d764730228433a593
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-06-10 23:23:46 +0200

    Improvements
    The parser only saves the top 5 scores.

--- BurgerTime/HighScoreParser.cpp
+++ BurgerTime/HighScoreParser.cpp
@@ -1,23 +1,25 @@
 ﻿#include "HighScoreParser.h"
 
 #include <fstream>
-#include <ostream>
 
 #pragma warning(disable : 4996)
-#include <document.h>
 #include <writer.h>
+#include <stringbuffer.h>
 #pragma warning(default : 4996)
+
+#include <Logger.h>
 
 int HighScoreParser::ParseHighScores(const std::string& fileName)
 {
     using namespace rapidjson;
 
-    std::string filePath = "../Data/" + fileName;
-    std::ifstream inputFile(filePath);
+    m_FileName = "../Data/" + fileName;
+    std::ifstream inputFile(m_FileName);
 
     // Check if file was opened successfully
     if (!inputFile.is_open()) 
     {
+        GenerateDefaultJson(m_FileName);
         return 0;
     }
 
@@ -28,19 +30,20 @@ int HighScoreParser::ParseHighScores(const std::string& fileName)
     // Close the input file
 	inputFile.close();
 
-    Document document;
-    document.Parse(fileContents.c_str());
+    m_Document.Parse(fileContents.c_str());
 
-    if (document.HasParseError() || !document.IsArray()) 
+    if (m_Document.HasParseError() || !m_Document.IsArray())
     {
+        GenerateDefaultJson(m_FileName);
         return 0;
     }
 
+
     int biggestHighScore = 0;
 
-    for (SizeType i = 0; i < document.Size(); ++i) 
+    for (SizeType i = 0; i < m_Document.Size(); ++i)
     {
-        const Value& scoreObj = document[i];
+        const Value& scoreObj = m_Document[i];
         if (scoreObj.IsObject() && scoreObj.HasMember("name") && scoreObj.HasMember("high_score")) 
         {
             const Value& nameValue = scoreObj["name"];
@@ -60,4 +63,86 @@ int HighScoreParser::ParseHighScores(const std::string& fileName)
     }
 
     return biggestHighScore;
+}
+
+void HighScoreParser::UpdateHighScores(const std::string& playerName, int score)
+{
+    m_HighScores[playerName] = std::max(m_HighScores[playerName], score);
+
+    using namespace rapidjson;
+
+
+    Document::AllocatorType& allocator = m_Document.GetAllocator();
+
+    //Value newPlayer(kObjectType);
+
+    // Only keep the top 5 in the file.
+	m_Document.Clear();
+
+	for (const auto& [newName, newScore] : GetTopFive())
+    {
+        Value newPlayer(kObjectType);
+        newPlayer.AddMember("name", Value(newName.c_str(), allocator).Move(), allocator);
+        newPlayer.AddMember("high_score", newScore, allocator);
+        m_Document.PushBack(newPlayer, allocator);
+    }
+
+
+    StringBuffer bufferOut;
+    Writer<StringBuffer> writer(bufferOut);
+    m_Document.Accept(writer);
+
+    std::ofstream outFile(m_FileName);
+    if (!outFile.is_open()) 
+        return;
+
+    outFile.write(bufferOut.GetString(), bufferOut.GetSize());
+    outFile.close();
+}
+
+std::vector<std::pair<std::string, int>> HighScoreParser::GetTopFive()
+{
+    std::vector<std::pair<std::string, int>> sortedVector(m_HighScores.begin(), m_HighScores.end());
+
+    std::ranges::sort(sortedVector, [](const auto& a, const auto& b)
+        {
+            return a.second > b.second;
+        });
+
+    std::vector<std::pair<std::string, int>> topFive;
+    int count = 0;
+
+    for (const auto& entry : sortedVector)
+    {
+        topFive.emplace_back(entry);
+        count++;
+
+        if (count == 5)
+        {  
+            break;
+        }
+    }
+
+    return topFive;
+}
+
+void HighScoreParser::GenerateDefaultJson(const std::string& filename)
+{
+    using namespace rapidjson;
+
+    std::ofstream file(filename);
+    if (!file.is_open()) 
+    {
+        real::Logger::LogError("HighScoreParser => Failed to create JSON file {}", filename);
+    	return;
+    }
+
+    m_Document.SetArray();
+
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    m_Document.Accept(writer);
+
+    file.write(buffer.GetString(), buffer.GetSize());
+    file.close();
 }
--- BurgerTime/HighScoreParser.h
+++ BurgerTime/HighScoreParser.h
@@ -3,15 +3,28 @@
 
 #include <map>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <ostream>
 
+#pragma warning(disable : 4996)
+#include <document.h>
+#pragma warning(default : 4996)
 
 class HighScoreParser final
 {
 public:
 	static int ParseHighScores(const std::string& fileName);
+	static void UpdateHighScores(const std::string& playerName, int score);
+	static std::vector<std::pair<std::string, int>> GetTopFive();
 
 private:
+	static void GenerateDefaultJson(const std::string& filename);
+
 	inline static std::map<std::string, int> m_HighScores{};
+	inline static rapidjson::Document m_Document{};
+	inline static std::string m_FileName{};
 };
 
 #endif // HIGHSCOREPARSER_H
