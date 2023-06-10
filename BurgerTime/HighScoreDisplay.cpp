f3af51874d90fcf3dc2c7b9259ac7061301a5f00
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-06-10 18:26:41 +0200

    Added HighScoreDisplay
    The highscore is read from a json and overwritten once the current player has a bigger score.

---
+++ BurgerTime/HighScoreDisplay.cpp
@@ -0,0 +1,28 @@
+﻿#include "HighScoreDisplay.h"
+
+#include <TextComponent.h>
+#include "HighScoreParser.h"
+
+HighScoreDisplay::HighScoreDisplay(real::GameObject* pOwner, ScoreDisplay* pScoreDisplay)
+	: Component(pOwner)
+	, m_ScoreDisplay(pScoreDisplay)
+{
+	m_HighScore = HighScoreParser::ParseHighScores("highscores.json");
+	GetOwner()->GetComponent<real::TextComponent>()->SetText(std::to_string(m_HighScore));
+
+	m_ScoreDisplay->scoreChanged.AddObserver(this);
+}
+
+HighScoreDisplay::~HighScoreDisplay()
+{
+	m_ScoreDisplay->scoreChanged.RemoveObserver(this);
+}
+
+void HighScoreDisplay::HandleEvent(int newScore)
+{
+	if (newScore > m_HighScore)
+	{
+		m_HighScore = newScore;
+		GetOwner()->GetComponent<real::TextComponent>()->SetText(std::to_string(m_HighScore));
+	}
+}
---
+++ BurgerTime/HighScoreDisplay.h
@@ -0,0 +1,30 @@
+﻿#ifndef HIGHSCOREDISPLAY_H
+#define HIGHSCOREDISPLAY_H
+
+#include <Component.h>
+
+#include "ScoreDisplay.h"
+#include <Observer.h>
+
+class HighScoreDisplay final : public real::Component
+	, public real::Observer<int>
+{
+public:
+	explicit HighScoreDisplay(real::GameObject* pOwner, ScoreDisplay* pScoreDisplay);
+	virtual ~HighScoreDisplay() override;
+	HighScoreDisplay(const Component& other) = delete;
+	HighScoreDisplay operator=(const Component& rhs) = delete;
+	HighScoreDisplay(Component&& other) = delete;
+	HighScoreDisplay operator=(Component&& rhs) = delete;
+	
+	void Update() override {}
+	bool CanRender() const override { return false; }
+
+	void HandleEvent(int) override;
+	void OnSubjectDestroy() override {}
+private:
+	ScoreDisplay* m_ScoreDisplay;
+	int m_HighScore{};
+};
+
+#endif // HIGHSCOREDISPLAY_H
