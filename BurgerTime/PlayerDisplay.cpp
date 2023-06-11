1874cda11c455b6327812117f99700b0006da923
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-06-11 22:38:04 +0200

    Update PlayerDisplay.cpp

--- BurgerTime/PlayerDisplay.cpp
+++ BurgerTime/PlayerDisplay.cpp
@@ -23,34 +23,40 @@ void PlayerDisplay::HandleEvent(int player)
 	const auto pMiddleFont = real::ResourceManager::GetInstance().LoadFont("fonts/8-bit-hud.ttf", 16);
 	const auto pFont = real::ResourceManager::GetInstance().LoadFont("fonts/8-bit-hud.ttf", 10);
 
-	using alignment = real::TextComponent::Alignment;
+	using alignment = real::TextComponent::HorizontalAlignment;
 
 	const auto playerText = GetOwner()->CreateGameObject();
 	playerText->AddComponent<real::TextureComponent>();
 	playerText->AddComponent<real::TextComponent>()->SetFont(pMiddleFont);
 	playerText->GetComponent<real::TextComponent>()->SetText("Player " + std::to_string(player));
-	playerText->GetComponent<real::TextComponent>()->ChangeAlignment(alignment::center);
+	playerText->GetComponent<real::TextComponent>()->ChangeHorizontalAlignment(alignment::center);
 
 	const auto nameText = playerText->CreateGameObject();
 	nameText->AddComponent<real::TextureComponent>();
 	nameText->AddComponent<real::TextComponent>()->SetFont(pFont);
-	nameText->GetComponent<real::TextComponent>()->ChangeAlignment(alignment::center);
+	nameText->GetComponent<real::TextComponent>()->ChangeHorizontalAlignment(alignment::center);
 
 	switch (player)
 	{
 	case 1:
 	{
-		playerText->GetComponent<real::TransformComponent>()->SetLocalPosition(/*m_Settings.width / 8.f*/0, 50);
-		nameText->GetComponent<real::TransformComponent>()->SetLocalPosition(/*m_Settings.width / 8.f*/0, 40);
+		playerText->GetComponent<real::TransformComponent>()->SetLocalPosition(180, 50);
+		nameText->GetComponent<real::TransformComponent>()->SetLocalPosition(0, 40);
 		nameText->GetComponent<real::TextComponent>()->SetText("Peter Pepper");
 		break;
 	}
 	case 2:
 	{
-		playerText->GetComponent<real::TransformComponent>()->SetLocalPosition(235, 50);
+		GetOwner()->GetChildAt(0)->GetComponent<real::TransformComponent>()->SetLocalPosition(0, 50);
+		GetOwner()->GetChildAt(0)->GetComponent<real::TextComponent>()->ChangeHorizontalAlignment(alignment::center);
+		GetOwner()->GetChildAt(0)->GetChildAt(0)->GetComponent<real::TransformComponent>()->SetLocalPosition(0, 40);
+		GetOwner()->GetChildAt(0)->GetChildAt(0)->GetComponent<real::TextComponent>()->SetText("Peter Pepper");
+
+		playerText->GetComponent<real::TransformComponent>()->SetLocalPosition(310, 50);
 		nameText->GetComponent<real::TransformComponent>()->SetLocalPosition(15, 40);
 		nameText->GetComponent<real::TextComponent>()->SetText("Sally Salt");
 		break;
 	}
+	default: ;
 	}
 }
