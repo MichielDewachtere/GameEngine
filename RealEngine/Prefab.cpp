2235600f2d3c208b22055b0cd10c698b46dc4d8b
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-11-23 21:31:49 +0100

    Added Prefab base classs

---
+++ RealEngine/Prefab.cpp
@@ -0,0 +1,14 @@
+﻿#include "Prefab.h"
+
+#include "GameObject.h"
+#include "Scene.h"
+
+real::Prefab::Prefab(Scene& scene)
+	: m_pGameObject(scene.CreateGameObject())
+{
+}
+
+real::Prefab::Prefab(GameObject& gameObject)
+	: m_pGameObject(gameObject.CreateGameObject())
+{
+}
---
+++ RealEngine/Prefab.h
@@ -0,0 +1,31 @@
+﻿#ifndef PREFAB_H
+#define PREFAB_H
+
+
+namespace real
+{
+	class GameObject;
+	class Scene;
+
+	class Prefab
+	{
+	public:
+		explicit Prefab(Scene& scene);
+		explicit Prefab(GameObject& gameObject);
+		virtual ~Prefab() = default;
+
+		Prefab(const Prefab& other) = delete;
+		Prefab& operator=(const Prefab& rhs) = delete;
+		Prefab(Prefab&& other) = delete;
+		Prefab& operator=(Prefab&& rhs) = delete;
+
+		GameObject& GetGameObject() const { return *m_pGameObject; }
+
+	protected:
+		GameObject* m_pGameObject;
+
+		//virtual void InitComponents() = 0;
+	};
+}
+
+#endif // PREFAB_H
