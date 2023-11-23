1b97c09ed88f2515025fab6e3b2d22c3f76ba0d1
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-11-23 21:35:50 +0100

    Added Rectangle component
    This component draws a rectangle

---
+++ RealEngine/Rectangle.cpp
@@ -0,0 +1,23 @@
+﻿#include "Rectangle.h"
+
+#include "SDLRenderer.h"
+
+real::Rectangle::Rectangle(GameObject* pOwner)
+	: Component(pOwner)
+{
+}
+
+void real::Rectangle::Render() const
+{
+	SDLRenderer::GetInstance().RenderRectangle(m_Rectangle, m_IsFilled, m_Color);
+}
+
+void real::Rectangle::SetRect(SDL_Rect rect)
+{
+	m_Rectangle = rect;
+}
+
+void real::Rectangle::SetRect(int x, int y, int width, int height)
+{
+	SetRect(SDL_Rect{ x, y, width, height });
+}
---
+++ RealEngine/Rectangle.h
@@ -0,0 +1,41 @@
+﻿#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include <SDL_rect.h>
+
+#include "Component.h"
+
+namespace real
+{
+	class Rectangle final : public Component
+	{
+	public:
+		explicit Rectangle(GameObject* pOwner);
+		virtual ~Rectangle() override = default;
+
+		Rectangle(const Rectangle& other) = delete;
+		Rectangle& operator=(const Rectangle& rhs) = delete;
+		Rectangle(Rectangle&& other) = delete;
+		Rectangle& operator=(Rectangle&& rhs) = delete;
+
+		void Update() override {}
+
+		bool CanRender() const override { return true; }
+		void Render() const override;
+
+		void SetRect(SDL_Rect rect);
+		void SetRect(int x, int y, int width, int height);
+
+		SDL_Rect GetRectangle() const { return m_Rectangle; }
+
+		void SetIsFilled(bool isFilled) { m_IsFilled = isFilled; }
+		void SetColor(SDL_Color color) { m_Color = color; }
+
+	private:
+		SDL_Rect m_Rectangle{};
+		bool m_IsFilled{ true };
+		SDL_Color m_Color{ 255,255,255,255 };
+	};
+}
+
+#endif // RECTANGLE_H
