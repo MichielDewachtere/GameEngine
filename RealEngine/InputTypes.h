a61079442da5ed6304283bc650806e84b1748b20
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-11-28 16:19:51 +0100

    Added Input structs
    Added two input structs, one for computer input (keyboard and mouse) and one for controller. This is more manageable than the previous solution.

---
+++ RealEngine/InputTypes.h
@@ -0,0 +1,32 @@
+﻿#ifndef INPUTTYPES_H
+#define INPUTTYPES_H
+
+#include <memory>
+#include "Command.h"
+#include "XInputController.h"
+
+namespace real
+{
+	struct ComputerInput
+	{
+		uint32_t event, scancode;
+		std::unique_ptr<Command> command;
+
+		ComputerInput(uint32_t event, uint32_t scancode, Command* command)
+			: event(event), scancode(scancode), command(command) {}
+
+	};
+
+	struct ControllerInput
+	{
+		uint8_t controller;
+		XInputController::ControllerButton button;
+		XInputController::InputType inputType;
+		std::unique_ptr<Command> command;
+
+		ControllerInput(uint8_t controller, XInputController::ControllerButton button, XInputController::InputType inputType, Command* command)
+			: controller(controller), button(button), inputType(inputType), command(command) {}
+	};
+}
+
+#endif // INPUTTYPES_H
