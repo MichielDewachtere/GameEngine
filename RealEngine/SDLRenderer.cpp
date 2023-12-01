72820cf4d3309007baf35de45473d42e29a1f5f9
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-12-01 10:00:36 +0100

    Added SDLRenderer
    Default render system for the engine using SDL2

--- RealEngine/SDLRenderer.cpp
+++ RealEngine/SDLRenderer.cpp
@@ -4,7 +4,7 @@
 #include <stdexcept>
 
 #include "SceneManager.h"
-#include "Texture2D.h"
+#include "SDLTexture2D.h"
 
 int GetOpenGLDriverIndex()
 {
@@ -20,13 +20,17 @@ int GetOpenGLDriverIndex()
 	return openglIndex;
 }
 
+real::SDLRenderer::SDLRenderer(SDL_Window* window, glm::u8vec4 background)
+{
+	m_BackgroundColor = background;
+	Init(window, glmToSDLColor(background));
+}
+
 void real::SDLRenderer::Init(SDL_Window* window, SDL_Color background)
 {
-	m_window = window;
-	m_clearColor = background;
-
-	m_renderer = SDL_CreateRenderer(window, GetOpenGLDriverIndex(), SDL_RENDERER_ACCELERATED);
-	if (m_renderer == nullptr) 
+	m_pWindow = window;
+	m_pRenderer = SDL_CreateRenderer(window, GetOpenGLDriverIndex(), SDL_RENDERER_ACCELERATED);
+	if (m_pRenderer == nullptr) 
 	{
 		throw std::runtime_error(std::string("SDL_CreateRenderer Error: ") + SDL_GetError());
 	}
@@ -39,17 +43,17 @@ void real::SDLRenderer::Init(SDL_Window* window, SDL_Color background)
 #endif // USE_IMGUI
 }
 
-void real::SDLRenderer::Render()
+void real::SDLRenderer::Render() const
 {
-	const auto& color = GetBackgroundColor();
-	SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, color.a);
-	SDL_RenderClear(m_renderer);
+	//const auto& color = GetBackgroundColor();
+	SDL_SetRenderDrawColor(m_pRenderer, m_BackgroundColor.r, m_BackgroundColor.g, m_BackgroundColor.b, m_BackgroundColor.a);
+	SDL_RenderClear(m_pRenderer);
 
 	SceneManager::GetInstance().Render();
 
 #ifdef USE_IMGUI
 	ImGui_ImplOpenGL2_NewFrame();
-	ImGui_ImplSDL2_NewFrame(m_window);
+	ImGui_ImplSDL2_NewFrame(m_pWindow);
 	ImGui::NewFrame();
 
 	//some logic
@@ -60,8 +64,9 @@ void real::SDLRenderer::Render()
 	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 #endif // USE_IMGUI
 
-	SDL_RenderPresent(m_renderer);
+	SDL_RenderPresent(m_pRenderer);
 }
+
 
 void real::SDLRenderer::Destroy()
 {
@@ -71,78 +76,138 @@ void real::SDLRenderer::Destroy()
 	ImGui::DestroyContext();
 #endif // USE_IMGUI
 
-	if (m_renderer != nullptr)
+	if (m_pRenderer != nullptr)
 	{
-		SDL_DestroyRenderer(m_renderer);
-		m_renderer = nullptr;
+		SDL_DestroyRenderer(m_pRenderer);
+		m_pRenderer = nullptr;
 	}
 }
 
 void real::SDLRenderer::RenderTexture(const Texture2D& texture, const float x, const float y) const
 {
-	SDL_Rect dst{};
-	dst.x = static_cast<int>(x);
-	dst.y = static_cast<int>(y);
-	SDL_QueryTexture(texture.GetSDLTexture(), nullptr, nullptr, &dst.w, &dst.h);
-	SDL_RenderCopy(GetSDLRenderer(), texture.GetSDLTexture(), nullptr, &dst);
+	RenderTexture(texture, x, y, static_cast<float>(texture.GetSize().x), static_cast<float>(texture.GetSize().y));
 }
 
 void real::SDLRenderer::RenderTexture(const Texture2D& texture, const float x, const float y, const float width, const float height) const
 {
+	const auto sdlTexture = dynamic_cast<const SDLTexture2D*>(&texture);
+
+	if (sdlTexture == nullptr)
+		return;
+
 	SDL_Rect dst;
 	dst.x = static_cast<int>(x);
 	dst.y = static_cast<int>(y);
 	dst.w = static_cast<int>(width);
 	dst.h = static_cast<int>(height);
-	SDL_RenderCopy(GetSDLRenderer(), texture.GetSDLTexture(), nullptr, &dst);
+	SDL_RenderCopy(GetSDLRenderer(), sdlTexture->GetSDLTexture(), nullptr, &dst);
 }
 
-void real::SDLRenderer::RenderRectangle(const SDL_Rect& rect, bool isFilled, SDL_Color color) const
+void real::SDLRenderer::RenderSprite(const Texture2D& texture, glm::ivec4 src, glm::ivec4 dst, float angle, 
+									glm::ivec2 center, RenderFlip flip)
+{
+	const SDL_Rect srcRect = glmToSDLRect(src);
+	const SDL_Rect dstRect = glmToSDLRect(dst);
+
+	const SDL_Point rotationCenter = glmToSDLPoint(center);
+
+	SDL_RendererFlip sdlFlip = SDL_FLIP_NONE;
+	switch (flip)
+	{
+	case RenderFlip::vertical: 
+		sdlFlip = SDL_FLIP_VERTICAL;
+		break;
+	case RenderFlip::horizontal:
+		sdlFlip = SDL_FLIP_HORIZONTAL;
+		break;
+	case RenderFlip::none:
+		sdlFlip = SDL_FLIP_NONE;
+		break;
+	}
+
+	SDL_RenderCopyEx(GetSDLRenderer(), dynamic_cast<const SDLTexture2D*>(&texture)->GetSDLTexture(), &srcRect, &dstRect, angle, &rotationCenter, sdlFlip);
+}
+
+void real::SDLRenderer::RenderRectangle(const glm::ivec4& rect, bool isFilled, glm::u8vec4 color) const
 {
 	SDL_Color originalColor;
-	SDL_GetRenderDrawColor(m_renderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
+	SDL_GetRenderDrawColor(m_pRenderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
 
-	SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, 255);
+	const SDL_Rect sdlRect{ rect.x, rect.y, rect.z, rect.w };
+
+	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, color.a);
 	if (isFilled)
-		SDL_RenderFillRect(GetSDLRenderer(), &rect);
+		SDL_RenderFillRect(GetSDLRenderer(), &sdlRect);
 	else
-		SDL_RenderDrawRect(GetSDLRenderer(), &rect);
-	SDL_SetRenderDrawColor(m_renderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+		SDL_RenderDrawRect(GetSDLRenderer(), &sdlRect);
+	SDL_SetRenderDrawColor(m_pRenderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
 }
 
-void real::SDLRenderer::RenderPoint(const int x, const int y, SDL_Color color) const
+void real::SDLRenderer::RenderPoint(int x, int y, glm::u8vec4 color) const
 {
 	SDL_Color originalColor;
-	SDL_GetRenderDrawColor(m_renderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
+	SDL_GetRenderDrawColor(m_pRenderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
 
-	SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, 255);
+	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, color.a);
 	SDL_RenderDrawPoint(GetSDLRenderer(), x, y);
-	SDL_SetRenderDrawColor(m_renderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+	SDL_SetRenderDrawColor(m_pRenderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
 }
 
-void real::SDLRenderer::RenderLine(const int x1, const int y1, const int x2, const int y2, SDL_Color color) const
+void real::SDLRenderer::RenderLine(int x1, int y1, int x2, int y2, glm::u8vec4 color) const
 {
 	SDL_Color originalColor;
-	SDL_GetRenderDrawColor(m_renderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
+	SDL_GetRenderDrawColor(m_pRenderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
 
-	SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, 255);
+	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, 255);
 	SDL_RenderDrawLine(GetSDLRenderer(), x1, y1, x2, y2);
-	SDL_SetRenderDrawColor(m_renderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+	SDL_SetRenderDrawColor(m_pRenderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
 }
 
-void real::SDLRenderer::RenderShape(const SDL_Point* points, int count, SDL_Color color) const
+void real::SDLRenderer::RenderShape(const glm::ivec2* points, int count, glm::u8vec4 color) const
 {
 	SDL_Color originalColor;
-	SDL_GetRenderDrawColor(m_renderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
+	SDL_GetRenderDrawColor(m_pRenderer, &originalColor.r, &originalColor.g, &originalColor.b, &originalColor.a);
 
-	SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, 255);
-	SDL_RenderDrawLines(m_renderer, points, count);
-	SDL_SetRenderDrawColor(m_renderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+	// Transform glm::ivec2 to SDL_Point
+	const auto sdlPoints = new SDL_Point[count];
+	for (int i = 0; i < count; ++i) 
+		sdlPoints[i] = glmToSDLPoint(points[i]);
+	
+	SDL_SetRenderDrawColor(m_pRenderer, color.r, color.g, color.b, 255);
+	SDL_RenderDrawLines(m_pRenderer, sdlPoints, count);
+	SDL_SetRenderDrawColor(m_pRenderer, originalColor.r, originalColor.g, originalColor.b, originalColor.a);
 }
 
-void real::SDLRenderer::RenderShape(const std::vector<SDL_Point>& points, SDL_Color color) const
+void real::SDLRenderer::RenderShape(const std::vector<glm::ivec2>& points, glm::u8vec4 color) const
 {
 	RenderShape(&points.front(), static_cast<int>(points.size()), color);
 }
 
-inline SDL_Renderer* real::SDLRenderer::GetSDLRenderer() const { return m_renderer; }
+SDL_Point real::SDLRenderer::glmToSDLPoint(const glm::ivec2& point)
+{
+	SDL_Point sdlPoint;
+	sdlPoint.x = point.x;
+	sdlPoint.y = point.y;
+	return sdlPoint;
+}
+
+SDL_Rect real::SDLRenderer::glmToSDLRect(const glm::ivec4& rect)
+{
+	SDL_Rect sdlRect;
+	sdlRect.x = rect.x;
+	sdlRect.y = rect.y;
+	sdlRect.w = rect.z;
+	sdlRect.h = rect.w;
+	return sdlRect;
+}
+
+SDL_Color real::SDLRenderer::glmToSDLColor(const glm::u8vec4& color)
+{
+	SDL_Color sdlColor;
+	sdlColor.r = color.x;
+	sdlColor.g = color.y;
+	sdlColor.b = color.z;
+	sdlColor.a = color.w;
+	return sdlColor;
+
+}
--- RealEngine/SDLRenderer.h
+++ RealEngine/SDLRenderer.h
@@ -6,42 +6,52 @@
 #include <vector>
 #include <glm/vec2.hpp>
 
-#include "Singleton.h"
+#include "Renderer.h"
 
 namespace real
 {
-	class Texture2D;
-	/**
-	 * Simple RAII wrapper for the SDL renderer
-	 */
-	class SDLRenderer final : public Singleton<SDLRenderer>
+	class SDLTexture2D;
+
+	class SDLRenderer final : public Renderer
 	{
-		SDL_Renderer* m_renderer{};
-		SDL_Window* m_window{};
-		SDL_Color m_clearColor{};
+	public:
+		SDLRenderer(SDL_Window* window, glm::u8vec4 background);
+		virtual ~SDLRenderer() override = default;
+
+		SDLRenderer(const SDLRenderer& other) = delete;
+		SDLRenderer operator=(const SDLRenderer& rhs) = delete;
+		SDLRenderer(SDLRenderer&& other) = delete;
+		SDLRenderer operator=(SDLRenderer&& rhs) = delete;
+
+		void Init(SDL_Window* window, SDL_Color background);
+		//void Render();
+		virtual void Render() const override;
+		virtual void Destroy() override;
+
+		virtual void RenderTexture(const Texture2D& texture, float x, float y) const override;
+		virtual void RenderTexture(const Texture2D& texture, float x, float y, float width, float height) const override;
+
+		void RenderSprite(const Texture2D& texture, glm::ivec4 src, glm::ivec4 dst, float angle = 0, glm::ivec2 center = { 0,0 },
+		                  RenderFlip flip = RenderFlip::none) override;
+
+		virtual void RenderRectangle(const glm::ivec4& rect, bool isFilled, glm::u8vec4 color) const override;
+		virtual void RenderPoint(int x, int y, glm::u8vec4 color) const override;
+		virtual void RenderLine(int x1, int y1, int x2, int y2, glm::u8vec4 color) const override;
+		virtual void RenderShape(const glm::ivec2* points, int count, glm::u8vec4 color) const override;
+		virtual void RenderShape(const std::vector<glm::ivec2>& points, glm::u8vec4 color) const override;
+
+		SDL_Renderer* GetSDLRenderer() const { return m_pRenderer; }
+
+	private:
+		SDL_Renderer* m_pRenderer{ nullptr };
+		SDL_Window* m_pWindow{ nullptr };
 
 		//ImGui
 		bool m_ShowDemo{ false };
-	public:
-		void Init(SDL_Window* window, SDL_Color background);
-		void Render();
-		void Destroy();
 
-		void RenderTexture(const Texture2D& texture, float x, float y) const;
-		void RenderTexture(const Texture2D& texture, float x, float y, float width, float height) const;
-
-		void RenderRectangle(const SDL_Rect& rect, bool isFilled, SDL_Color color) const;
-		void RenderPoint(int x, int y, SDL_Color color) const;
-		void RenderPoint(const glm::ivec2 pos, SDL_Color color) const { RenderPoint(pos.x, pos.y, color); }
-		void RenderLine(int x1, int y1, int x2, int y2, SDL_Color color) const;
-		void RenderLine(const glm::ivec2 begin, const glm::ivec2 end, SDL_Color color) const { RenderLine(begin.x, begin.y, end.x, end.y, color); }
-		void RenderShape(const SDL_Point* points, int count, SDL_Color color) const;
-		void RenderShape(const std::vector<SDL_Point>& points, SDL_Color color) const;
-
-		SDL_Renderer* GetSDLRenderer() const;
-
-		const SDL_Color& GetBackgroundColor() const { return m_clearColor; }
-		void SetBackgroundColor(const SDL_Color& color) { m_clearColor = color; }
+		static SDL_Point glmToSDLPoint(const glm::ivec2& point);
+		static SDL_Rect glmToSDLRect(const glm::ivec4& rect);
+		static SDL_Color glmToSDLColor(const glm::u8vec4& color);
 	};
 }
 
