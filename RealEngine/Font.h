8f9f432b67cb34bbda3f12fb22bf8aa220486849
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-12-01 09:54:10 +0100

    Added a Font default class
    Different font types can be derived based on the render system. For example, the default render system is SDLRenderer, so the default font class will be SDLFont

--- RealEngine/Font.h
+++ RealEngine/Font.h
@@ -1,28 +1,20 @@
 #ifndef FONT_H
 #define FONT_H
 
+#include <memory>
 #include <string>
+#include <glm/vec4.hpp>
 
-struct _TTF_Font;
+#include "Texture2D.h"
+
 namespace real
 {
-	/**
-	 * Simple RAII wrapper for a _TTF_Font
-	 */
-	class Font final
+	class Font
 	{
 	public:
-		_TTF_Font* GetFont() const;
-		explicit Font(const std::string& fullPath, unsigned int size);
-		~Font();
+		virtual ~Font() = default;
 
-		Font(const Font &) = delete;
-		Font(Font &&) = delete;
-		Font & operator= (const Font &) = delete;
-		Font & operator= (const Font &&) = delete;
-	private:
-		_TTF_Font* m_font;
-		unsigned int m_size;
+		virtual std::shared_ptr<Texture2D> CreateTexture(const std::string& text, glm::u8vec4 color) = 0;
 	};
 }
 
