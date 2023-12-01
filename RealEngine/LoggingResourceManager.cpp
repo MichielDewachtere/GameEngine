e1f61bca79f5a477c136a735406962ce2103736c
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-12-01 09:57:38 +0100

    Added L ogging Resource Manager
    Logging ResourceManager logs every resource that is being loaded from a given resource manager system. Typically used when debugging.

---
+++ RealEngine/LoggingResourceManager.cpp
@@ -0,0 +1,15 @@
+﻿#include "LoggingResourceManager.h"
+
+#include "Logger.h"
+
+std::shared_ptr<real::Texture2D> real::LoggingResourceManager::LoadTexture(const std::string& file) const
+{
+	Logger::LogInfo("Loaded texture from file: {}", file);
+	return m_RealResourceManager->LoadTexture(file);
+}
+
+std::shared_ptr<real::Font> real::LoggingResourceManager::LoadFont(const std::string& file, unsigned size) const
+{
+	Logger::LogInfo("Loaded font from file: {}, with size {}", file, size);
+	return m_RealResourceManager->LoadFont(file, size);
+}
---
+++ RealEngine/LoggingResourceManager.h
@@ -0,0 +1,26 @@
+﻿#ifndef LOGGINGRESOURCEMANAGER_H
+#define LOGGINGRESOURCEMANAGER_H
+
+#include "ResourceManager.h"
+
+namespace real
+{
+	class LoggingResourceManager final : public ResourceManager
+	{
+	public:
+		explicit LoggingResourceManager(ResourceManager* resourceManager) : m_RealResourceManager(resourceManager) {}
+		virtual ~LoggingResourceManager() override = default;
+
+		LoggingResourceManager(const LoggingResourceManager& other) = delete;
+		LoggingResourceManager& operator=(const LoggingResourceManager& rhs) = delete;
+		LoggingResourceManager(LoggingResourceManager&& other) = delete;
+		LoggingResourceManager& operator=(LoggingResourceManager&& rhs) = delete;
+
+		std::shared_ptr<Texture2D> LoadTexture(const std::string& file) const override;
+		std::shared_ptr<Font> LoadFont(const std::string& file, unsigned size) const override;
+	private:
+		std::unique_ptr<ResourceManager> m_RealResourceManager;
+	};
+}
+
+#endif // LOGGINGRESOURCEMANAGER_H
