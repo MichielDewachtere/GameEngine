9afee9ed52f67b5a74eaef1a1af0eb37583833c6
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-12-01 09:57:04 +0100

    Added Logging Renderer
    Logging Renderer logs every render behaviour from a given render system. Typically used when debugging.

---
+++ RealEngine/LoggingRenderer.cpp
@@ -0,0 +1,63 @@
+﻿#include "LoggingRenderer.h"
+
+#include "Logger.h"
+#include "SDLTexture2D.h"
+
+void real::LoggingRenderer::Render() const
+{
+	m_RealRenderer->Render();
+}
+
+void real::LoggingRenderer::Destroy()
+{
+	m_RealRenderer->Destroy();
+	Logger::LogInfo("Destroyed Renderer");
+}
+
+void real::LoggingRenderer::RenderTexture(const Texture2D& texture, float x, float y) const
+{
+	m_RealRenderer->RenderTexture(texture, x, y);
+	//Logger::LogInfo("Rendered texture {}, at position {}; {}", &texture, x, y);
+}
+
+void real::LoggingRenderer::RenderTexture(const Texture2D& texture, float x, float y, float width, float height) const
+{
+	m_RealRenderer->RenderTexture(texture, x, y, width, height);
+	//Logger::LogInfo("Rendered texture {}, at position {}; {}, with width {} and height {}", &texture, x, y, width , height);
+}
+
+void real::LoggingRenderer::RenderSprite(const Texture2D& texture, glm::ivec4 src, glm::ivec4 dst, float angle, glm::ivec2 center, RenderFlip flip)
+{
+	m_RealRenderer->RenderSprite(texture, src, dst, angle, center, flip);
+	//Logger::LogInfo("Rendered sprite {}, at position {}; {}, with width {} and height {}", &texture, dst.x, dst.y);
+}
+
+void real::LoggingRenderer::RenderRectangle(const glm::ivec4& rect, bool isFilled, glm::u8vec4 color) const
+{
+	m_RealRenderer->RenderRectangle(rect, isFilled, color);
+	//Logger::LogInfo("Rendered a rectangle to the screen at position {}; {} and dimensions {}; {}", rect.x, rect.y, rect.z, rect.w);
+}
+
+void real::LoggingRenderer::RenderPoint(int x, int y, glm::u8vec4 color) const
+{
+	m_RealRenderer->RenderPoint(x, y, color);
+	//Logger::LogInfo("Rendered a point to the screen at position {}; {}", x, y);
+}
+
+void real::LoggingRenderer::RenderLine(int x1, int y1, int x2, int y2, glm::u8vec4 color) const
+{
+	m_RealRenderer->RenderLine(x1, y1, x2, y2, color);
+	//Logger::LogInfo("Rendered a line to the screen from {}; {}, to {}; {}", x1, y1, x2, y2);
+}
+
+void real::LoggingRenderer::RenderShape(const glm::ivec2* points, int count, glm::u8vec4 color) const
+{
+	m_RealRenderer->RenderShape(points, count, color);
+	//Logger::LogInfo("Rendered a shape to the screen at position {}; {}, containing {} points", points[0].x, points[0].y, count);
+}
+
+void real::LoggingRenderer::RenderShape(const std::vector<glm::ivec2>& points, glm::u8vec4 color) const
+{
+	m_RealRenderer->RenderShape(points, color);
+	//Logger::LogInfo("Rendered a shape to the screen at position {}; {}, containing {} points", points[0].x, points[0].y, points.size());
+}
---
+++ RealEngine/LoggingRenderer.h
@@ -0,0 +1,43 @@
+﻿#ifndef LOGGINGRENDERER_H
+#define LOGGINGRENDERER_H
+
+#include <memory>
+
+#include "Renderer.h"
+
+namespace real
+{
+	class LoggingRenderer final : public Renderer
+	{
+	public:
+		explicit LoggingRenderer(Renderer* pRenderer) : m_RealRenderer(pRenderer) {}
+		virtual ~LoggingRenderer() override = default;
+
+		LoggingRenderer(const LoggingRenderer& other) = delete;
+		LoggingRenderer& operator=(const LoggingRenderer& rhs) = delete;
+		LoggingRenderer(LoggingRenderer&& other) = delete;
+		LoggingRenderer& operator=(LoggingRenderer&& rhs) = delete;
+
+		void Render() const override;
+		void Destroy() override;
+
+		void RenderTexture(const Texture2D& texture, float x, float y) const override;
+		void RenderTexture(const Texture2D& texture, float x, float y, float width, float height) const override;
+
+		void RenderSprite(const Texture2D& texture, glm::ivec4 src, glm::ivec4 dst, float angle = 0, glm::ivec2 center = { 0,0 },
+		                  RenderFlip flip = RenderFlip::none) override;
+
+		void RenderRectangle(const glm::ivec4& rect, bool isFilled, glm::u8vec4 color) const override;
+		void RenderPoint(int x, int y, glm::u8vec4 color) const override;
+		void RenderLine(int x1, int y1, int x2, int y2, glm::u8vec4 color) const override;
+		void RenderShape(const glm::ivec2* points, int count, glm::u8vec4 color) const override;
+		void RenderShape(const std::vector<glm::ivec2>& points, glm::u8vec4 color) const override;
+
+		Renderer& GetRenderer()const { return *m_RealRenderer; }
+
+	private:
+		std::unique_ptr<Renderer> m_RealRenderer;
+	};
+}
+
+#endif // LOGGINGRENDERER_H
