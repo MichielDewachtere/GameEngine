e77293cc9f83cce269e170ebea9254138c354967
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2023-12-01 09:55:27 +0100

    Added multiple systems
    Render system and Resource Sytem

--- RealEngine/Locator.cpp
+++ RealEngine/Locator.cpp
@@ -9,7 +9,25 @@ void real::Locator::RegisterAudioSystem(Audio* audio)
 		m_AudioSystem.reset(audio);
 }
 
+void real::Locator::RegisterRenderSystem(Renderer* renderer)
+{
+	if (renderer == nullptr)
+		m_RenderSystem.reset(&m_DefaultRenderSystem);
+	else
+		m_RenderSystem.reset(renderer);
+}
+
+void real::Locator::RegisterResourceSystem(ResourceManager* resourceManager)
+{
+	if (resourceManager == nullptr)
+		m_RenderSystem.reset(&m_DefaultRenderSystem);
+	else
+		m_ResourceSystem.reset(resourceManager);
+}
+
 void real::Locator::ShutDown()
 {
 	m_AudioSystem.reset(&m_DefaultAudioSystem);
+	m_RenderSystem.reset(&m_DefaultRenderSystem);
+	m_ResourceSystem.reset(&m_DefaultResourceSystem);
 }
--- RealEngine/Locator.h
+++ RealEngine/Locator.h
@@ -6,18 +6,36 @@
 #include "Audio.h"
 #include "NullAudio.h"
 
+#include "Renderer.h"
+#include "NullRenderer.h"
+
+#include "ResourceManager.h"
+#include "NullResourceManager.h"
+
 namespace real
 {
-    class Locator final
+	class Locator final
     {
     public:
         static Audio& GetAudioSystem() { return *m_AudioSystem; }
         static void RegisterAudioSystem(Audio* audio);
 
-        static void ShutDown();
+        static Renderer& GetRenderSystem() { return *m_RenderSystem; }
+        static void RegisterRenderSystem(Renderer* renderer);
+
+        static ResourceManager& GetResourceSystem() { return *m_ResourceSystem; }
+        static void RegisterResourceSystem(ResourceManager* resourceManager);
+
+		static void ShutDown();
     private:
         inline static std::unique_ptr<Audio> m_AudioSystem = nullptr;
         inline static NullAudio m_DefaultAudioSystem{};
+
+        inline static std::unique_ptr<Renderer> m_RenderSystem = nullptr;
+        inline static NullRenderer m_DefaultRenderSystem{};
+
+        inline static std::unique_ptr<ResourceManager> m_ResourceSystem = nullptr;
+        inline static NullResourceManager m_DefaultResourceSystem{};
     };
 }
 #endif // LOCATOR_H
